160086077ec53f57193fcfd765346b26160ffc5f
Vyacheslav chopenko <syavchik260119901@gmail.com>
2025-05-09 01:00:11 +0300

    feat: add tasks 1–8, clean up starter files

---
+++ src/task-1.ts
@@ -0,0 +1,23 @@
+// У цьому фрагменті коду оголошені три змінні з різними типами значень: текст, число і булеве значення.
+
+// const name = "Alice";
+// const age = 30;
+// const isOnline = true;
+
+// console.log(`Name: ${name}`);
+// console.log(`Age: ${age}`);
+// console.log(`Online: ${isOnline}`);
+
+// Завдання:
+
+// Додай до кожної змінної явну типізацію.
+// Переконайся, що TypeScript правильно визначає тип, і не видає помилку.
+// Залиш назви змінних і їх значення без змін.
+
+const name: string = "Alice";
+const age: number = 30;
+const isOnline: boolean = true;
+
+console.log(`Name: ${name}`);
+console.log(`Age: ${age}`);
+console.log(`Online: ${isOnline}`);
